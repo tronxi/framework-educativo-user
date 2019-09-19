309ab0a71ac4cb72bc8e256b43f039a9e5b34749
tronxi <sergio.gsanchez97@gmail.com>
2019-09-20 00:04:37 +0200

    first commit

---
+++ src/main/java/es/upm/frameworkeducativo/FrameworkEducativoApplication.java
@@ -0,0 +1,13 @@
+package es.upm.frameworkeducativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkEducativoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkEducativoApplication.class, args);
+    }
+
+}
