94fa722083767abb060d35c4201b4a7bed36c556
tronxi <sergio.gsanchez97@gmail.com>
2019-10-19 20:19:46 +0200

    add ident and password change boolean

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,10 +3,10 @@ insert into `ROLE` values ('1', 'TEACHER');
 insert into `ROLE` values ('2', 'STUDENT');
 
 -- contrasena 123
-insert into `USER` values ('0', 'alumno', '', '$2a$04$xfqwcWepMIM7/gHiDoWt2.wxMr2rZQTG1.gwBpFL4qDXSGXO9tZNm', 'alumn@upm');
-insert into `USER` values ('1', 'profesor', '', '$2a$04$xfqwcWepMIM7/gHiDoWt2.wxMr2rZQTG1.gwBpFL4qDXSGXO9tZNm', 'profesor@upm');
-insert into `USER` values ('2', 'admin', '', '$2a$04$xfqwcWepMIM7/gHiDoWt2.wxMr2rZQTG1.gwBpFL4qDXSGXO9tZNm', 'admin@upm');
+insert into `USER` (IDENT, NAME, SURNAMES, PASSWORD, EMAIL, IS_CHANGED) values ('BM0567', 'alumno', '', '$2a$04$xfqwcWepMIM7/gHiDoWt2.wxMr2rZQTG1.gwBpFL4qDXSGXO9tZNm', 'alumn@upm', false);
+insert into `USER` (IDENT, NAME, SURNAMES, PASSWORD, EMAIL, IS_CHANGED) values ('15500384v', 'profesor', '', '$2a$04$xfqwcWepMIM7/gHiDoWt2.wxMr2rZQTG1.gwBpFL4qDXSGXO9tZNm', 'profesor@upm', false);
+insert into `USER` (IDENT, NAME, SURNAMES, PASSWORD, EMAIL, IS_CHANGED) values ('000000', 'admin', '', '$2a$04$xfqwcWepMIM7/gHiDoWt2.wxMr2rZQTG1.gwBpFL4qDXSGXO9tZNm', 'admin@upm', false);
 
-insert into `USER_ROLE` values ('0', '2');
-insert into `USER_ROLE` values ('1', '1');
-insert into `USER_ROLE` values ('2', '0');
+insert into `USER_ROLE` values ('1', '2');
+insert into `USER_ROLE` values ('2', '1');
+insert into `USER_ROLE` values ('3', '0');
