36255c15be617036a63bbc011c3b94bd498277cf
tronxi <sergio.gsanchez97@gmail.com>
2019-10-24 21:24:20 +0200

    changed database and add json properties

--- src/main/java/es/upm/frameworkeducativo/infrastructure/api/rest/model/UserDTO.java
+++ src/main/java/es/upm/frameworkeducativo/infrastructure/api/rest/model/UserDTO.java
@@ -1,14 +1,14 @@
 package es.upm.frameworkeducativo.infrastructure.api.rest.model;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
 
 import java.util.List;
 
-@AllArgsConstructor
-@Value
 @Builder
+@Data
 public class UserDTO {
     private String id_user;
     private String ident;
@@ -18,4 +18,22 @@ public class UserDTO {
     private String email;
     private List<String> roles;
     private Boolean isChanged;
+    @JsonCreator
+    public UserDTO(@JsonProperty("id_user")String id_user,
+                   @JsonProperty("ident")String ident,
+                   @JsonProperty("name")String name,
+                   @JsonProperty("surnames")String surnames,
+                   @JsonProperty("password")String password,
+                   @JsonProperty("email")String email,
+                   @JsonProperty("roles")List<String> roles,
+                   @JsonProperty("isChanged")Boolean isChanged) {
+        this.id_user = id_user;
+        this.ident = ident;
+        this.name = name;
+        this.surnames = surnames;
+        this.password = password;
+        this.email = email;
+        this.roles = roles;
+        this.isChanged = isChanged;
+    }
 }
